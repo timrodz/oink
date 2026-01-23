5a61f2583c952f4058017b92dc9f9337ab761aac
Juan Morais <timrodz@icloud.com>
2026-01-24 12:17:59 +1300

    refactor: Update constants

--- src-tauri/src/constants.rs
+++ src-tauri/src/constants.rs
@@ -1,4 +1,4 @@
-pub const FRANKFURTER_BASE_URL: &str = "https://api.frankfurter.dev/v1/{}..{}?base={}&symbols={}";
+pub const FRANKFURTER_BASE_URL: &str = "https://api.frankfurter.dev/v1";
+pub const FRANKFURTER_DATE_FORMAT: &str = "%Y-%m-%d";
 pub const DECEMBER: u32 = 12;
 pub const MONTHS_PER_YEAR: u32 = 12;
-pub const FRANKFURTER_DATE_FORMAT: &str = "%Y-%m-%d";
